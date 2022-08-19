b73c669e614e2497d49573ccbe805a618007f2ef
Borschik0 <72044379+Borschik0@users.noreply.github.com>
2022-08-19 19:15:05 +0300

    Update 6

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/screens/product_detail_screen.dart';
-import 'package:shop_app/screens/product_overview_screen.dart';
+import 'package:provider/provider.dart';
+import './providers/products.dart';
+import './screens/product_detail_screen.dart';
+import './screens/product_overview_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,16 +13,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final ThemeData theme =
         ThemeData(primarySwatch: Colors.purple, fontFamily: 'Lato');
-    return MaterialApp(
-      title: 'MyShop',
-      theme: theme.copyWith(
-        colorScheme:
-            theme.colorScheme.copyWith(secondary: Colors.deepOrangeAccent),
+    return ChangeNotifierProvider(
+      create: (cxt) => Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: theme.copyWith(
+          colorScheme:
+              theme.colorScheme.copyWith(secondary: Colors.deepOrangeAccent),
+        ),
+        home: const ProductOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => const ProductDetailScreen(),
+        },
       ),
-      home: ProductOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (ctx) => const ProductDetailScreen(),
-      },
     );
   }
 }
---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/product.dart';
+import '../providers/products.dart';
+import 'product_item.dart';
+
+class ProductsGrid extends StatelessWidget {
+  const ProductsGrid({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.items;
+    return GridView.builder(
+        padding: const EdgeInsets.all(10.0),
+        itemCount: products.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10),
+        itemBuilder: (ctx, i) => ProductItem(id: products[i].id, title: products[i].title, imageUrl: products[i].imageUrl));
+  }
+}
