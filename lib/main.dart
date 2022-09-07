0f1f68fee7273d0f1d1f5472d678a39cea77ff95
Borschik0 <72044379+Borschik0@users.noreply.github.com>
2022-09-07 21:43:08 +0300

    Working with Multiple Providers

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import './providers/products.dart';
 import './screens/product_detail_screen.dart';
 import './screens/product_overview_screen.dart';
+import 'providers/cart.dart';
 
 void main() => runApp(const MyApp());
 
@@ -13,8 +14,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final ThemeData theme =
         ThemeData(primarySwatch: Colors.purple, fontFamily: 'Lato');
-    return ChangeNotifierProvider(
-      create: (cxt) => Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (cxt) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (cxt) => Cart(),
+        ),
+      ],
       child: MaterialApp(
         title: 'MyShop',
         theme: theme.copyWith(
