8444388ebddeb44284aeec863f33919faf941b0f
Borschik0 <72044379+Borschik0@users.noreply.github.com>
2022-12-08 21:50:23 +0200

    207. Making Cart Items Dismissible

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,30 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/cart.dart';
 
 class CartItem extends StatelessWidget {
   final String id;
+  final String productId;
   final double price;
   final int quantity;
   final String title;
 
-  const CartItem({required this.id, required this.price, required this.quantity, required this.title, Key? key}) : super(key: key);
+  const CartItem({required this.id, required this.productId, required this.price, required this.quantity, required this.title, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(
-        vertical: 4,
-        horizontal: 15
+    return Dismissible(
+      key: ValueKey(id),
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: const Icon(Icons.delete, color: Colors.white, size: 40),
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20),
+        margin: const EdgeInsets.symmetric(
+            vertical: 4,
+            horizontal: 15
+        ),
       ),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListTile(
-          leading: CircleAvatar(child: Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: FittedBox(child: Text('\$$price')),
-          )),
-          title: Text(title),
-          subtitle: Text('Total: \$${price * quantity}'),
-          trailing: Text('$quantity x'),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction){
+        Provider.of<Cart>(context, listen: false).removeItem(productId);
+      },
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: CircleAvatar(child: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: FittedBox(child: Text('\$$price')),
+            )),
+            title: Text(title),
+            subtitle: Text('Total: \$${price * quantity}'),
+            trailing: Text('$quantity x'),
+          ),
         ),
       ),
     );
