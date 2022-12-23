57aff3f8f432e10cc293e1ea77afb41b01864aa9
Borschik0 <vitalinastepnikova@gmail.com>
2022-12-23 15:33:10 +0200

    213. Making Orders Expandable & Stateful Widgets vs Providers

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,22 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'dart:math';
 
 import '../providers/orders.dart' as ord;
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
   const OrderItem({required this.order, Key? key}) : super(key: key);
 
+  @override
+  State<OrderItem> createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
   @override
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.all(10),
       child: Column(
         children: <Widget>[
-          ListTile(title: Text('\$${order.amount}'),
-          subtitle: Text(DateFormat('dd/MM/yyyy hh:mm').format(order.dateTime)),
-          trailing:  IconButton(icon: const Icon(Icons.expand_more), onPressed: (){
-
-          },),)
+          ListTile(title: Text('\$${widget.order.amount}'),
+          subtitle: Text(DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime)),
+          trailing:  IconButton(icon: Icon( _expanded ? Icons.expand_less : Icons.expand_more), onPressed: (){
+            setState(() {
+              _expanded =!_expanded;
+            });
+          },),),
+          if (_expanded) Container( padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+            height: min(widget.order.products.length * 20.0 + 10, 180),
+          child: ListView(children: widget.order.products.map((prod) => Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(prod.title, style: const TextStyle(
+                fontSize: 18, fontWeight: FontWeight.bold
+              )),
+              Text('${prod.quantity}x \$${prod.price}', style: const TextStyle(
+                fontSize: 18,
+                color: Colors.grey
+              ),)
+            ],
+          )).toList(),),)
         ],
       ),
     );
