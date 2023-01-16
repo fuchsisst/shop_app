876b936ac341df831788230a5fd08a469d571543
Borschik0 <vitalinastepnikova@gmail.com>
2023-01-16 15:35:50 +0200

    247. Handling Errors Gracefully

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -81,6 +81,9 @@ class Products with ChangeNotifier {
           imageUrl: product.imageUrl);
       _items.add(newProduct);
       notifyListeners();
+    }).catchError((error){
+      print(error);
+      throw error;
     });
   }
 
